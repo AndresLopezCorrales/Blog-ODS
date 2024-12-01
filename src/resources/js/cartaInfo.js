befbdb8fff6916952b42feca443bd579fdcf7b0f
Andrés López Corrales <andreslopezcorrales326@gmail.com>
2024-11-30 20:34:30 -0700

    Agregué Comentarios

--- src/resources/js/cartaInfo.js
+++ src/resources/js/cartaInfo.js
@@ -3,7 +3,7 @@ const articulo = document.getElementById('articulos');
 const popup = document.getElementById('mi-popup');
 const closeBtn = document.getElementById('close-btn');
 
-// Mostrar el pop-up cuando se hace clic en el artículo
+// Mostrar el pop-up cuando se hace clic en div-blog
 articulo.addEventListener('click', function (event) {
     if (event.target.tagName === 'ARTICLE') {
         popup.style.display = 'block';
@@ -26,22 +26,24 @@ popup.addEventListener('click', function (event) {
 articulo.addEventListener('click', function (event) {
     // Verifica que el elemento clicado sea un artículo
     if (event.target.tagName === 'ARTICLE') {
-        // Obtener el valor del atributo title
+        //Obtener todos los data-
         const titleValue = event.target.title;
         const dataInfo = event.target.getAttribute('data-info');
         const dataInfoBueno = dataInfo.replace(/\n/g, '<br>');
         const dataPredeterminado = event.target.getAttribute('data-predeterminado');
         const dataImagen = event.target.getAttribute('data-imagen');
 
-        // Mostrar el valor en el párrafo
+        // Mostrar el valor en el 'textoMostrar'
         const resultado = document.getElementById('textoMostrar');
 
+        //Si no tiene imagen el div-blog
         if (dataImagen === 'null') {
             resultado.innerHTML = `<h1 class="p-12 text-center justify-center text-5xl text-white"> ${titleValue} </h1> 
                                     <h2 class="p-6 text-center justify-center text-2xl text-white">${dataPredeterminado}</h2> <br> 
                                     <h3 class="p-6 mx-8 text-justify justify-center text-xl text-white">${dataInfoBueno}</h3>`;
         }
 
+        //Si sí tiene imagen
         else {
             resultado.innerHTML = `<h1 class="p-12 text-center justify-center text-5xl text-white"> ${titleValue} </h1> 
                                     <h2 class="p-6 text-center justify-center text-2xl text-white">${dataPredeterminado}</h2> <br> 
